309e7f1f3cfe21dafd9676c91e7a39de72e7e271
pongkaruina <31153888+auey@users.noreply.github.com>
2019-03-13 15:31:57 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// Reply with two static messages
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {YBchlMDT59Wz1ewMy93JkqbcSmoxxnNcWLyz0KFD1hn9CWJMwVxW9BB3HN/K4fOA+ljULtnw7gou7/BTOBWggrCI2ZHB8siSXwwGTJ6PrDy/y3fMsqkk7uNvZUk6tsNeQ97ZF6wKv25z/8GzPV5YXgdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
